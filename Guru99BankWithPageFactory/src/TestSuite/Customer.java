aee37aabb844387df4213b91cc4c032f1ecae259
divya <divya@DESKTOP-OEG3EP3>
2018-07-30 19:06:19 +0530

    test

--- Guru99BankWithPageFactory/src/TestSuite/Customer.java
+++ Guru99BankWithPageFactory/src/TestSuite/Customer.java
@@ -14,6 +14,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
